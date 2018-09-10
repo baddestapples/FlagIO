99791361f4478fdfa339d2c16f52a36da56dd8d8
Jon Carvajal <scio777@hotmail.com>
2018-09-09 21:21:16 -0400

    Running out of one project now. Will fix later after game is done.

---
+++ FlagIO/static/js/game.js
@@ -0,0 +1,32 @@
+﻿var socket = io();
+
+socket.on('message', function (data) {
+  console.log(data);
+});
+
+//Initialize player
+var player = new Player(socket);
+
+var canvas = document.getElementById('canvas');
+
+var context = canvas.getContext('2d');
+
+var sprite = new Image();
+sprite.src = 'static/images/knight.png';
+
+socket.on('players', function (players) {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'green';
+  for (var id in players) {
+    var player = players[id];
+    //context.beginPath();
+
+    context.drawImage(sprite, player.x, player.y);
+
+    //context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.fill();
+  }
+});
